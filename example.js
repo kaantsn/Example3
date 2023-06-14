8685aa3e2e5e13223d01efe15f9ad3fcedf3cc5a
Kaan Tosun <tr.kaantsn@gmail.com>
2023-06-14 21:39:14 +0300

    first commit

---
+++ example.js
@@ -0,0 +1,18 @@
+// This is a simple example of JavaScript code
+
+// Declare and initialize variables
+var firstName = "John";
+var lastName = "Doe";
+var age = 25;
+
+// Print out the values of the variables
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Age: " + age);
+
+// Calculate the year of birth
+var currentYear = new Date().getFullYear();
+var yearOfBirth = currentYear - age;
+
+// Print out the year of birth
+console.log("Year of Birth: " + yearOfBirth);
